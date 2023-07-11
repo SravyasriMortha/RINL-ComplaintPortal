9949c7ac17264bf1615020b665edca3442c1e7c3
SravyasriMortha <sravyasri_2101cs74@iitp.ac.in>
2023-07-11 19:04:40 +0530

    Complaint portal

---
+++ models/registers.js
@@ -0,0 +1,29 @@
+/*const mongoose = require('mongoose')
+
+const employeeSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    id: {
+        type: Number,
+        required: true
+    },
+    phno: {
+        type: Number,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    ConfirmPassword: {
+        type: String,
+        required: true
+    }
+})
+
+const Register = new mongoose.model("Register", employeeSchema)
+
+module.exports = Register*/
